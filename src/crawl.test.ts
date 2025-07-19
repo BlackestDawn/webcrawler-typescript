8e2f1a28334ea0e5cf353314dcb792a82728b4b9
Alexander Stauch <blacke4dawn@gmail.com>
2025-07-19 15:52:13 +0200

    URL extraction

--- src/crawl.test.ts
+++ src/crawl.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { normalizeURL } from './crawl';
+import { getURLsFromHTML, normalizeURL } from './crawl.js';
 
 describe('normalizeURL', () => {
   it('should strip the protocol from the URL', () => {
@@ -35,5 +35,65 @@ describe('normalizeURL', () => {
     const actual = normalizeURL(input);
     const expected = 'blog.boot.dev/path';
     expect(actual).toBe(expected);
+  });
+});
+
+describe('getURLsFromHTML', () => {
+  it('should get absolute URLs from HTML body', () => {
+    const htmlBody = `
+      <html>
+        <body>
+          <a href="https://blog.boot.dev/path/"><span>Go to Boot.dev</span></a>
+        </body>
+      </html>
+    `;
+    const baseURL = 'https://blog.boot.dev';
+    const actual = getURLsFromHTML(htmlBody, baseURL);
+    const expected = ['https://blog.boot.dev/path/'];
+    expect(actual).toEqual(expected);
+  });
+
+  it('should get relative URLs from HTML body', () => {
+    const htmlBody = `
+      <html>
+        <body>
+          <a href="/path/one"><span>Path One</span></a>
+          <a href="path/two"><span>Path Two</span></a>
+        </body>
+      </html>
+    `;
+    const baseURL = 'https://blog.boot.dev';
+    const actual = getURLsFromHTML(htmlBody, baseURL);
+    const expected = ['https://blog.boot.dev/path/one', 'https://blog.boot.dev/path/two'];
+    expect(actual.sort()).toEqual(expected.sort());
+  });
+
+  it('should get both absolute and relative URLs', () => {
+    const htmlBody = `
+      <html>
+        <body>
+          <a href="https://blog.boot.dev/path1/"><span>Path One</span></a>
+          <a href="/path2/"><span>Path Two</span></a>
+        </body>
+      </html>
+    `;
+    const baseURL = 'https://blog.boot.dev';
+    const actual = getURLsFromHTML(htmlBody, baseURL);
+    const expected = ['https://blog.boot.dev/path1/', 'https://blog.boot.dev/path2/'];
+    expect(actual.sort()).toEqual(expected.sort());
+  });
+
+  it('should not include javascript invokation', () => {
+    const htmlBody = `
+      <html>
+        <body>
+          <a href="javascript:void(0);"><span>Invalid URL</span></a>
+        </body>
+      </html>
+    `;
+    const baseURL = 'https://blog.boot.dev';
+    const actual = getURLsFromHTML(htmlBody, baseURL);
+    const expected: string[] = [];
+    expect(actual).toEqual(expected);
   });
 });
--- src/crawl.ts
+++ src/crawl.ts
@@ -1,4 +1,21 @@
+import { JSDOM } from 'jsdom';
+
 export function normalizeURL(url: string): string {
   let normalizedURL = new URL(url.toLowerCase());
   return (`${normalizedURL.hostname}${normalizedURL.pathname}`).replace(/\/+$/, '');
+}
+
+export function getURLsFromHTML(html: string, baseURL: string): string[] {
+  const dom = new JSDOM(html);
+  const urls: string[] = [];
+  const links = dom.window.document.querySelectorAll('a');
+  for (const link of links) {
+    const href = link.getAttribute('href');
+    if (href) {
+      const url = new URL(href, baseURL);
+      if (url.protocol === 'javascript:') continue;
+      urls.push(url.toString());
+    }
+  }
+  return urls;
 }

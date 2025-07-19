018b702137dcb5900b980ac1c2e6b760529cf362
Alexander Stauch <blacke4dawn@gmail.com>
2025-07-19 16:02:13 +0200

    invokation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+import { getURLsFromHTML } from "./crawl";
+import { argv } from "node:process";
+
+function main() {
+  if (argv.length < 3) {
+    console.error('Need to provide a URL to crawl');
+    process.exit(1);
+  }
+  if (argv.length > 3) {
+    console.error('Too many arguments');
+    process.exit(1);
+  }
+  const url = argv[2];
+  console.log(`Crawling ${url}`);
+  process.exit(0);
+}
+
+main();
